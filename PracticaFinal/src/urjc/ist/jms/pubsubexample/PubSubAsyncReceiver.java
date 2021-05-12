7b169e484591b828ed9ab766d298af60aded075c
ORTEGA APARICIO, JUAN ANTONIO <ja.ortega.2017@alumnos.urjc.es>
2021-05-12 14:46:08 +0000

    Añadiendo javadoc y nuevas constantes

--- PracticaFinal/src/urjc/ist/jms/pubsubexample/PubSubAsyncReceiver.java
+++ PracticaFinal/src/urjc/ist/jms/pubsubexample/PubSubAsyncReceiver.java
@@ -1,61 +1,104 @@
 package urjc.ist.jms.pubsubexample;
 
-import java.util.Objects;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 import javax.jms.*;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 
-public class PubSubAsyncReceiver implements Runnable, MessageListener{
 
-	private TopicConnection connection;
-	private Topic topic;
-	private boolean stopFlag;
+/**
+ * <h1>TestPubSubAsyncReceiver class</h1>
+ * 
+ * <p> The TestPubSubAsyncReceiver class launches N concurrent subscribers for 
+ * the publisher/subscriber pattern.
+ * <p>
+ * @authors César Borao Moratinos & Juan Antonio Ortega Aparicio
+ * @version 1.0, 11/05/2021
+ */
+public class TestPubSubAsyncReceiver {
+	
+	private static final int    NSUBS       = 3;           // Number of subscribers
+	private static final String factoryName = "Factoria2"; // Name of our factory
+	private static final String topicName   = "Topic1";    // Name of our topic
+	
+	private static ExecutorService ReceiverPool = Executors.newFixedThreadPool(NSUBS); // Pool of threads
 
-	public PubSubAsyncReceiver(TopicConnection con, Topic topic){
-		this.connection = con;
-		this.topic = topic;
-	}
-
-	@Override
-	public void run(){
-		try {   
-			TopicSession session = 
-					connection.createTopicSession(false, TopicSession.AUTO_ACKNOWLEDGE);
-			TopicSubscriber subscriber = session.createSubscriber(topic);
-			subscriber.setMessageListener(this);
-			connection.start();
-			System.out.println("Thread " + Thread.currentThread().getId() + " subscribed!");
-			
-			
-			while (!stopFlag) {
-				Thread.sleep(1000);
+	
+	/**
+	 * Method to close the ExecutorService in two stages: first avoiding running new
+	 * tasks in the pool and after, requesting the tasks running to finish.
+	 * 
+	 * @param firstTimeout Timeout to the first waiting stage.
+	 * @param secondTimeout Timeout to the second waiting stage.
+	 */
+	private static void shutdownAndAwaitTermination(int firstTimeout, int secondTimeout) {
+		ReceiverPool.shutdown(); 
+		try {
+			// Waiting for all threads to finish their tasks
+			if (!ReceiverPool.awaitTermination(firstTimeout, TimeUnit.SECONDS)) {
+				
+				// If tasks are not finished then we have to force closure
+				System.err.println("Uncompleted tasks. forcing closure...");
+				ReceiverPool.shutdownNow(); 
+				
+				// If tasks are not finished again, we have to finish with some threads
+				if (!ReceiverPool.awaitTermination(secondTimeout, TimeUnit.SECONDS)) {
+					System.err.println("Unended thread pool");
+				}else {
+					System.out.println("All threads closed");
+				}
+				
+			}else {
+				System.out.println("All threads closed");
 			}
-			System.err.println("TRACE: Return Thread");
-			return;
 			
-		} catch (JMSException ex) {
-			ex.printStackTrace();
-		} catch (InterruptedException ex) {
+		} catch (InterruptedException ie) {
+			ReceiverPool.shutdownNow();
 			Thread.currentThread().interrupt();
 		}
 	}
+
 	
-	@Override
-	public void onMessage(Message msg) {
+	public static void main(String[] args) {
+
 		try {
-			TextMessage m = (TextMessage)msg;
+			// Context initialization
+			InitialContext jndi = new InitialContext();
 			
-			if (Objects.equals(m.getText(), "CLOSE")){
-				System.out.println("No more messages. Closing now!");
-				stopFlag = true; //Enable condition to stop current Thread
+			// Factory is referenced here by lookup function
+			TopicConnectionFactory factory = 
+					(TopicConnectionFactory)jndi.lookup(factoryName);
+			
+			// Topic is referenced here by lookup function
+			TopicConnection connection = factory.createTopicConnection(); 
+			Topic topic = (Topic)jndi.lookup(topicName); 
+			
+			// Launch NSUBS
+			for(int i = 0; i < NSUBS; i++) {
 				
-			} else {
-				System.out.println("Listener, Thread " + 
-						Thread.currentThread().getId() +
-						" message received: " + m.getText());
+				// Asynchronous Pub/Sub is created here 
+				PubSubAsyncReceiver receiver = new PubSubAsyncReceiver(connection, topic); 
+				
+				// Launch receiver
+				ReceiverPool.submit(receiver); 
 			}
 			
+			// Closing thread pool
+			shutdownAndAwaitTermination(60, 60);
+
+			// Close the connection
+			System.err.println("TRACE: Closing connection");
+			connection.close(); 
+			System.err.println("END");
+
+		} catch (NamingException ex) {
+			ex.printStackTrace();
 		} catch (JMSException ex) {
 			ex.printStackTrace();
 		}
 	}
-
 }
